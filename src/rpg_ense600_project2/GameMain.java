ef959acb9c7d7a1bb7e9dd1358b0ccf685f5a57a
Shaina Caballero <scba055@aucklanduni.ac.nz>
2024-10-22 20:50:33 +1300

    First Commit

---
+++ src/rpg_ense600_project2/GameMain.java
@@ -0,0 +1,16 @@
+package rpg_ense600_project2;
+
+/**
+ *
+ * @author ywj5422
+ */
+public class GameMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
